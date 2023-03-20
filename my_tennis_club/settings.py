2570b5b5ed4a14b4f061d7608a6b25ecf6d2c4a7
Husni Abad <husni.abad.97@gmail.com>
2023-03-21 04:50:11 +0530

    connect with AWS PostgreSQL

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'husniabad',
+        'PASSWORD': 'husniabad1997',
+        'HOST': 'my-tennis-club.cuvrlu7vss8y.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
