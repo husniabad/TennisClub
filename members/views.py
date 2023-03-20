4ddf63f100975b3ecfd46a77682466ae627b1788
Husni Abad <husni.abad.97@gmail.com>
2023-03-21 02:57:29 +0530

    first commit

---
+++ members/views.py
@@ -0,0 +1,40 @@
+# Python functions that takes http requests and returns http response
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+from django.db.models import Q
+
+
+def members(request):  # not necessary to be (members)
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+
+def testing(request):
+    mydata = Member.objects.all().order_by('firstname', '-id').values()
+    template = loader.get_template('template.html')
+    context = {
+        'mymembers': mydata,
+    }
+    return HttpResponse(template.render(context, request))
